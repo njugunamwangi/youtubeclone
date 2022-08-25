e41fc92d64f06e70d3ca27beb22fc2cb0ab33fc8
njugunamwangi <njugunamwangi.n@gmail.com>
2022-08-25 10:11:33 +0300

    setting studio layout

---
+++ backend/views/layouts/main.php
@@ -0,0 +1,47 @@
+<?php
+
+/** @var \yii\web\View $this */
+/** @var string $content */
+
+use backend\assets\AppAsset;
+use common\widgets\Alert;
+use yii\bootstrap5\Breadcrumbs;
+use yii\bootstrap5\Html;
+use yii\bootstrap5\Nav;
+use yii\bootstrap5\NavBar;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <?php $this->registerCsrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body>
+    <?php $this->beginBody() ?>
+
+        <div class="wrap h-100 d-flex flex-column">
+            <?php echo $this->render('_header') ?>
+
+            <main class="d-flex">
+                <?php echo $this->render('_sidebar') ?>
+
+                <div class="container p-3">
+                    <?= Breadcrumbs::widget([
+                        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                    ]) ?>
+                    <?= Alert::widget() ?>
+                    <?= $content ?>
+                </div>
+            </main>
+        </div>
+
+    <?php $this->endBody() ?>
+    </body>
+    </html>
+<?php $this->endPage();
---
+++ backend/views/site/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = Yii::$app->name;
+?>
+<div class="site-index">
+    Index
+</div>
